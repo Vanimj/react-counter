29194dc847becda9b3406fccbcc0fd80e388ec54
Vani <muppuruvani13@gmail.com>
2020-06-03 17:14:23 -0500

    This is my counterApp commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Counter from './Components/Counter';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+//import "./index.css";
+import "./App.css";
+import Counter from "./Components/Counter";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
+  function handleDecrement() {
+    setCount(count - 1);
+  }
+
+  function handleReset() {
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Counter App</h2>
-      </header>
-      <Counter/>
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <button className="btn" onClick={handleReset}>
+        RESET
+      </button>
     </div>
   );
 }
